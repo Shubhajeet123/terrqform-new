013eb7f3085b5aca6d252d3b5b1f48e13f2b1561
Shubhajeet <alfabita9938@gmail.com>
2024-12-02 22:06:09 +0530

    new uppdate

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -1,8 +1,12 @@
+// In below command we can createe S3 bucket
 resource "aws_s3_bucket" "dev" {
     bucket = "mybuckeetststst"
    
   
 }
+
+// Here we can enable s3 versioning in terraform
+
 resource "aws_s3_bucket_versioning" "versioning" {
     bucket = aws_s3_bucket.dev.id
     versioning_configuration {
